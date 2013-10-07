8cc3ed737b71b282b7397e2a5fb6b39f81e0160e
dcharles525 <dcharles525@gmail.com>
2013-10-07 17:30:30 -0400

    Added Functions

---
+++ Fucntions/funtions1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int findarea(int x, int y); 
+
+int main(void)
+{
+	
+	int one;
+	int two;
+	scanf("%d%d" , &one , &two);
+	printf("%d", findarea(one,two));
+	getch();
+	return 0;
+	
+}
+
+int findarea(int x, int y)
+{
+	
+	return x * y;
+	
+}
